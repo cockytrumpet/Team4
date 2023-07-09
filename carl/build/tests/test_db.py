616905a62f7eee3a34493f42317cf3ced387c065
taer7274 <taer7274@colorado.edu>
2023-07-09 21:58:42 +0000

    Mock postgres and first unit test

--- carl/build/tests/test_db.py
+++ carl/build/tests/test_db.py
@@ -1,6 +1,36 @@
 import unittest
+import os
+import psycopg2
+import testing.postgresql
+import frontend.helper as h
+from sqlalchemy import create_engine
+import psycopg2
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        # create a new PostgreSQL server
+        self.postgresql = testing.postgresql.Postgresql()
+        # connect to PostgreSQL
+        engine = create_engine(self.postgresql.url())
+        # use driver
+        self.db = psycopg2.connect(**self.postgresql.dsn())
+        # initialize database with schema and tables
+        with self.db.cursor() as cur:
+            cur.execute(self.read_file('./tests/init.sql'))
 
 
-def test_test():
-    assert 1==1
+    def read_file(self,path):
+        '''Reads in and returns the contents of a file'''
+        with open(path, 'r') as f:
+            return f.read()
+    
+    def tearDown(self):
+        self.db.close()
+        self.postgresql.stop()
+
+    def test_table_exists(self):
+        self.assertTrue(h.table_exists(self.db, 'tags'))
+        self.assertFalse(h.table_exists(self.db, 'fake'))
+
 
