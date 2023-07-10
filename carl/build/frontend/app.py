2266fd2ee7e7c522dcc5a026f4c9b62cb1927250
Adam Fowler <cockytrumpet@icloud.com>
2023-07-10 02:35:00 +0000

    fix app.py

--- carl/build/frontend/app.py
+++ carl/build/frontend/app.py
@@ -4,37 +4,38 @@ import flask
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.middleware.proxy_fix import ProxyFix
 from markupsafe import escape
-import helper as h
-from helper import get_db_connection, table_exists
+from helper import *
 
 app = Flask(__name__)
 # not really 'secret', using os.environment for this one breaks production
 app.config["SECRET_KEY"] = "df0331cefc6c2b9a5d0208a726a5d1c0fd37324feba25506"
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
-conn = h.get_db_connection()
+conn = get_db_connection()
 
 # routes
 
+
 @app.route("/")
 def index():
-    return render_template("index.html", page='home')
+    return render_template("index.html", page="home")
 
 
 @app.route("/resources")
 def resources():
-    conn = get_db_connection()
     if not table_exists(conn, "resources"):
-        return render_template("notable.html", error="Resources", page='resources')
+        return render_template(
+            "notable.html", error="Resources", page="resources"
+        )
     cur = conn.cursor()
     cur.execute(
         "SELECT DISTINCT 0 AS id, create_date, title, link, descr FROM resources ORDER BY create_date DESC;"
     )
     resources = cur.fetchall()
     cur.close()
-    conn.close()
-    return render_template("resources.html", books=tags, page='resources')
-
+    return render_template(
+        "resources.html", resources=resources, page="resources"
+    )
 
 
 @app.route("/resourceform", methods=("GET", "POST"))
@@ -43,25 +44,25 @@ def resourceform():
         title = escape(request.form["title"])
         link = escape(request.form["link"])
         descr = escape(request.form["descr"])
-        conn = h.get_db_connection()
-        h.add_to_resources(title, link, descr, conn)
+        conn = get_db_connection()
+        add_to_resources(title, link, descr, conn)
         return redirect(url_for("resources"))
-    return render_template("resource_form.html", page = 'resourceform')
+    return render_template("resource_form.html", page="resourceform")
 
 
 @app.route("/projects")
 def projects():
-    conn = get_db_connection()
     if not table_exists(conn, "projects"):
-        return render_template("notable.html", error="Projects", page='projects')
+        return render_template(
+            "notable.html", error="Projects", page="projects"
+        )
     cur = conn.cursor()
     cur.execute(
         "SELECT DISTINCT 0 as id, title, descr FROM projects ORDER BY title ASC;"
     )
     projects = cur.fetchall()
     cur.close()
-    conn.close()
-    return render_template("projects.html", books=tags, page='projects')
+    return render_template("projects.html", projects=projects, page="projects")
 
 
 @app.route("/projectform", methods=("GET", "POST"))
@@ -69,22 +70,22 @@ def projectform():
     if request.method == "POST":
         title = escape(request.form["title"])
         descr = escape(request.form["descr"])
-        h.add_to_projects(title, descr,conn)
+        add_to_projects(title, descr, conn)
         return redirect(url_for("projects"))
-    return render_template("project_form.html", page = 'projectform')
+    return render_template("project_form.html", page="projectform")
 
 
 @app.route("/tags")
 def tags():
-    conn = get_db_connection()
     if not table_exists(conn, "tags"):
-        return render_template("notable.html", error="Tags", page='tags')
+        return render_template("notable.html", error="Tags", page="tags")
     cur = conn.cursor()
-    cur.execute("SELECT DISTINCT 0 as id, title, descr FROM tags ORDER BY title ASC;")
+    cur.execute(
+        "SELECT DISTINCT 0 as id, title, descr FROM tags ORDER BY title ASC;"
+    )
     tags = cur.fetchall()
     cur.close()
-    conn.close()
-    return render_template("tags.html", books=tags, page = 'tags')
+    return render_template("tags.html", tags=tags, page="tags")
 
 
 @app.route("/tagform", methods=("GET", "POST"))
@@ -92,17 +93,20 @@ def tagform():
     if request.method == "POST":
         title = escape(request.form["title"])
         descr = escape(request.form["descr"])
-        h.add_to_tags(title, descr,conn)
+        add_to_tags(title, descr, conn)
         return redirect(url_for("tags"))
-    return render_template("tag_form.html", page = 'tagform')
+    return render_template("tag_form.html", page="tagform")
+
 
 def request_has_connection():
-    return hasattr(flask.g, 'dbconn')
+    return hasattr(flask.g, "dbconn")
+
 
 def get_request_connection():
     if not request_has_connection():
-        flask.g.dbconn = h.get_db_connection()
+        flask.g.dbconn = get_db_connection()
     return flask.g.dbconn
+
 
 @app.teardown_request
 def close_db_connection(ex):
@@ -110,5 +114,6 @@ def close_db_connection(ex):
         conn = get_request_connection()
         conn.close()
 
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=3000)
+    app.run(debug=True)
