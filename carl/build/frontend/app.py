1ba009b40e50676e21dbfe83828351f909f5cd3f
Nolan Ollada <nolan.ollada1@gmail.com>
2023-07-10 17:19:21 +0000

    add 404 error page

--- carl/build/frontend/app.py
+++ carl/build/frontend/app.py
@@ -97,6 +97,9 @@ def tagform():
         return redirect(url_for("tags"))
     return render_template("tag_form.html", page="tagform")
 
+@app.errorhandler(404)
+def page_not_found(error): 
+    return render_template('404.html'), 404
 
 def request_has_connection():
     return hasattr(flask.g, "dbconn")
