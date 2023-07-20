80271174e9cb0784186d4833d291de85ca7e4e01
Adam Fowler <cockytrumpet@icloud.com>
2023-07-20 19:26:50 +0000

    clear_db.py: fix to work with new tables

--- carl/clear_db.py
+++ carl/clear_db.py
@@ -3,7 +3,9 @@ from build.frontend.helper import get_db_connection
 conn = get_db_connection()
 cur = conn.cursor()
 
-cur.execute("drop table if exists resources, tags, projects, resource_tags;")
+cur.execute(
+    "drop table if exists project_resources, resource_tags, resources, tags, projects;"
+)
 
 conn.commit()
 cur.close()
