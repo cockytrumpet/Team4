a4d40f6354f8776758b8a210238ae24cf19773f6
Adam Fowler <cockytrumpet@icloud.com>
2023-07-30 04:18:54 +0000

    clear_db.py: make linter happy

--- carl/clear_db.py
+++ carl/clear_db.py
@@ -4,7 +4,14 @@ conn = get_db_connection()
 cur = conn.cursor()
 
 cur.execute(
-    "drop table if exists project_resources, project_tags, resource_tags, resources, tags, projects;"
+    """
+    drop table if exists project_resources, 
+                         project_tags, 
+                         resource_tags, 
+                         resources, 
+                         tags, 
+                         projects;
+    """
 )
 
 conn.commit()
